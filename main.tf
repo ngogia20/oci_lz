2115c5cb6405175dfe8bce362411db15ce256a58
nikesh_g_gogia <nikesh.g.gogia@oracle.com>
2022-10-12 14:57:02 +0530

    Initial Commit

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "http" {
-    address = "https://objectstorage.ap-mumbai-1.oraclecloud.com/p/kvsX5YUjS-037wVon_VTAaMuRVRw34wn0bgL4SByprNUbSpf7r9BeNN0ocx9FM3h/n/apaccpt03/b/tfstate/o/terraform.tfstate"
+    # address = "https://objectstorage.ap-mumbai-1.oraclecloud.com/p/kvsX5YUjS-037wVon_VTAaMuRVRw34wn0bgL4SByprNUbSpf7r9BeNN0ocx9FM3h/n/apaccpt03/b/tfstate/o/terraform.tfstate"
+    address = "https://objectstorage.ap-mumbai-1.oraclecloud.com//p/NbFoAmTdrZiiGzw89aObsJRnFnzc8XwyQdkSqxNSzj9Wc5OPpVVliZsTGQBQX57-/n/apaccpt03/b/pyt_exp/o/terraform.tfstate"
     update_method = "PUT"
   }
 }
