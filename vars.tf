6a6de8ce4c10f3a903a570743c688f7597670f80
ngogia20 <nikesh.gogia@gmail.com>
2022-10-17 23:56:36 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -4,4 +4,4 @@ variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaaodl6djtpaf
 variable tenancy_ocid {default = "ocid1.tenancy.oc1..aaaaaaaa5nras26nkpnlcd4g3ox6gfmzjaahxwb245stlrh7krkfrpu2nu5q"}
 variable user_ocid {default = "ocid1.user.oc1..aaaaaaaafaqlxrsudbbm56er7of43fifmx53lpzkc46jviapnw5bh6nnjgxa"}
 variable fingerprint {default = "8e:37:d7:82:88:02:1d:20:b1:6f:6d:a1:a3:61:b4:3e"}
-variable private_key_path {default = "/tmp/oci_api_key.pem"}
+variable private_key_path {default = "/tmp/oci_lz-master/oci_api_key.pem"}
